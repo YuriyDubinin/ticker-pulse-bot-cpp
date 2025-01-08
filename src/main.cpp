c6b561149c55dfea17fb676171b2e4e69ae5cb88
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-01-08 17:23:33 +0300

    Button templates

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,38 @@ int main() {
     fmt::print("[TICKER-PULSE-BOT]: started, version: {}\n", version);
 
     // API ключ
-    TgBot::Bot bot("API_KEY");
+    TgBot::Bot bot("");
 
     // Создание пула потоков с 2 потоками
     ThreadPool pool(2);
 
     // Обработка команды /start
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "📊 Привет! Я отслеживаю курсы криптовалют. 🚀");
+        TgBot::InlineKeyboardMarkup::Ptr keyboard(new TgBot::InlineKeyboardMarkup);
+
+        // Создание кнопок
+        TgBot::InlineKeyboardButton::Ptr button1(new TgBot::InlineKeyboardButton);
+        button1->text = "📉 Курс валют";
+        button1->callbackData = "ACTUAL";
+
+        // TgBot::InlineKeyboardButton::Ptr button2(new TgBot::InlineKeyboardButton);
+        // button2->text = "⬆️ Выбрать";
+        // button2->callbackData = "SELECT_CURRENCY";
+
+        TgBot::InlineKeyboardButton::Ptr button3(new TgBot::InlineKeyboardButton);
+        button3->text = "⚙️ Команды";
+        button3->callbackData = "ATIONS";
+
+        TgBot::InlineKeyboardButton::Ptr button4(new TgBot::InlineKeyboardButton);
+        button4->text = "ℹ️ Информация";
+        button4->callbackData = "INFO";
+
+        // Добавление кнопок в ряды
+        keyboard->inlineKeyboard.push_back({button1});
+        keyboard->inlineKeyboard.push_back({button3, button4});
+
+        // Отправка приветственного сообщения с клавиатурой
+        bot.getApi().sendMessage(message->chat->id, "📊 Привет! Я отслеживаю курсы криптовалют. 🚀", false, 0, keyboard);
     });
 
     // Обработка любых сообщений
@@ -37,6 +61,19 @@ int main() {
         pool.enqueueTask([&bot, message]() {
             bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
         });
+    });
+
+    // Обработка callback-данных
+    bot.getEvents().onCallbackQuery([&bot](TgBot::CallbackQuery::Ptr callbackQuery) {
+        if (callbackQuery->data == "ACTUAL") {
+            bot.getApi().sendMessage(callbackQuery->message->chat->id, "Список актуального курса топовых криптовалют"); // Пример ответа
+        } else if (callbackQuery->data == "SELECT_CURRENCY") {
+            bot.getApi().sendMessage(callbackQuery->message->chat->id, "Вызов меню выбора криптовалюты"); // Пример ответа
+        } else if (callbackQuery->data == "ATIONS") {
+            bot.getApi().sendMessage(callbackQuery->message->chat->id, "Список команд бота");
+        } else if (callbackQuery->data == "INFO") {
+            bot.getApi().sendMessage(callbackQuery->message->chat->id, "Обновления: Бот теперь поддерживает новые валюты.");
+        }
     });
 
     try {
