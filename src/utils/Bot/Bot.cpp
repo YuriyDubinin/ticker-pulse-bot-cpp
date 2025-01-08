9eb1029abedbf0360b51dccabe9e112c7d5eea18
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-01-08 19:28:16 +0300

    minor fix

---
+++ src/utils/Bot/Bot.cpp
@@ -0,0 +1,99 @@
+#include "Bot.h"
+
+// Bot.cpp
+#include "Bot.h"
+
+Bot::Bot(const std::string& token, int threadCount)
+    : bot(token), pool(threadCount) {}
+
+void Bot::start() {
+    // Обработка команды /start
+    bot.getEvents().onCommand("start", [this](TgBot::Message::Ptr message) {
+        onStartCommand(message);
+
+        std::string groupId = "@tickerpulse";
+        std::string testText = "Привет, группа! Это тестовое сообщение.";
+
+        sendToGroup(groupId, testText);
+    });
+
+    // Обработка любых сообщений
+    bot.getEvents().onAnyMessage([this](TgBot::Message::Ptr message) {
+        onAnyMessage(message);
+    });
+
+    // Обработка callback-данных
+    bot.getEvents().onCallbackQuery([this](TgBot::CallbackQuery::Ptr callbackQuery) {
+        onCallbackQuery(callbackQuery);
+    });
+
+    try {
+        // Печать имени бота
+        fmt::print("[TICKER-PULSE-BOT]: TG username - {}\n", bot.getApi().getMe()->username);
+
+        // Запуск Long Polling
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            fmt::print("[TICKER-PULSE-BOT]: Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        fmt::print("[TICKER-PULSE-BOT]: {}\n", e.what());
+    }
+};
+
+void Bot::onStartCommand(TgBot::Message::Ptr message) {
+    TgBot::InlineKeyboardMarkup::Ptr keyboard(new TgBot::InlineKeyboardMarkup);
+
+    // Создание кнопок
+    TgBot::InlineKeyboardButton::Ptr button1(new TgBot::InlineKeyboardButton);
+    button1->text = "📉 Курс валют";
+    button1->callbackData = "ACTUAL";
+
+    TgBot::InlineKeyboardButton::Ptr button3(new TgBot::InlineKeyboardButton);
+    button3->text = "⚙️ Команды";
+    button3->callbackData = "ATIONS";
+
+    TgBot::InlineKeyboardButton::Ptr button4(new TgBot::InlineKeyboardButton);
+    button4->text = "ℹ️ Информация";
+    button4->callbackData = "INFO";
+
+    // Добавление кнопок в ряды
+    keyboard->inlineKeyboard.push_back({button1});
+    keyboard->inlineKeyboard.push_back({button3, button4});
+
+    // Отправка приветственного сообщения с клавиатурой
+    bot.getApi().sendMessage(message->chat->id, "📊 Привет! Я отслеживаю курсы криптовалют. 🚀", false, 0, keyboard);
+};
+
+void Bot::onAnyMessage(TgBot::Message::Ptr message) {
+    fmt::print("User wrote {}\n", message->text);
+
+    if (message->text.find("/start") != 0) {
+        // Добавляем задачу в пул потоков
+        pool.enqueueTask([this, message]() {
+            bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        });
+    }
+};
+
+void Bot::sendToGroup(const std::string& groupId, const std::string& messageText) {
+    try {
+        bot.getApi().sendMessage(groupId, messageText);
+        fmt::print("[TICKER-PULSE-BOT]: Message sent to group {}\n", groupId);
+    } catch (TgBot::TgException& e) {
+        fmt::print("[TICKER-PULSE-BOT]: Failed to send message to group {}: {}\n", groupId, e.what());
+    }
+};
+
+void Bot::onCallbackQuery(TgBot::CallbackQuery::Ptr callbackQuery) {
+    if (callbackQuery->data == "ACTUAL") {
+        bot.getApi().sendMessage(callbackQuery->message->chat->id, "Список актуального курса топовых криптовалют");
+    } else if (callbackQuery->data == "SELECT_CURRENCY") {
+        bot.getApi().sendMessage(callbackQuery->message->chat->id, "Вызов меню выбора криптовалюты");
+    } else if (callbackQuery->data == "ATIONS") {
+        bot.getApi().sendMessage(callbackQuery->message->chat->id, "Список команд бота");
+    } else if (callbackQuery->data == "INFO") {
+        bot.getApi().sendMessage(callbackQuery->message->chat->id, "Обновления: Бот теперь поддерживает новые валюты.");
+    }
+};
---
+++ src/utils/Bot/Bot.h
@@ -0,0 +1,23 @@
+#ifndef BOT_H
+#define BOT_H
+
+#include <tgbot/tgbot.h>
+#include <fmt/core.h>
+#include <string>
+#include "../ThreadPool/ThreadPool.h"
+
+class Bot {
+    public:
+        Bot(const std::string& token, int threadCount);
+        void start();
+        void onStartCommand(TgBot::Message::Ptr message);
+        void onAnyMessage(TgBot::Message::Ptr message);
+        void sendToGroup(const std::string& groupId, const std::string& messageText);
+        void onCallbackQuery(TgBot::CallbackQuery::Ptr callbackQuery);
+
+    private:
+        TgBot::Bot bot;
+        ThreadPool pool;
+};
+
+#endif
