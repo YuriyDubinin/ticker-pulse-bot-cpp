0859acfe3bd69c43b148bc48131f1423602a99be
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-02-07 15:49:04 +0300

    Remove config directory from repository

--- config/global_config.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_CONFIG_H
-#define GLOBAL_CONFIG_H
-
-const std::string VERSION = "1.0.0";
-const std::string API_KEY = "7709079630:AAE06VwYO_l53Bns2Okn2QfCAjfJjEFRiAI";
-const std::string GROUP_ID = "@tickerpulse";
-const unsigned int CHECK_LIMIT_INTERVAL = 15;
-
-#endif
