4ad8605c9318dfd9405e7ec55ce9f37b43009501
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-06-26 18:04:58 +0300

    dependencies deleted

---
+++ include/postgres_connection.h
@@ -0,0 +1,26 @@
+#ifndef POSTGRESS_CONNECTION
+#define POSTGRESS_CONNECTION
+
+#include <libpq-fe.h>
+#include <string>
+
+class PostgresConnection {
+  public:
+    explicit PostgresConnection(const std::string& connection_info);
+    ~PostgresConnection();
+
+    PostgresConnection(const PostgresConnection&)            = delete;
+    PostgresConnection& operator=(const PostgresConnection&) = delete;
+
+    bool      isConnected() const;
+    PGresult* executeQuery(const std::string& query);
+    void      clearResult(PGresult* result);
+    void      close();
+
+    PGconn* raw(); // Доступ к PGconn* при необходимости
+
+  private:
+    PGconn* conn;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include "bot.h"
 #include "global_config.h"
+#include "postgres_connection.h"
 
 int main() {
     fmt::print("[TICKER_PULSE_BOT]: started, version: {}\n", VERSION);
 
     Bot bot(TELEGRAM_BOT_API_KEY, MAX_THREAD_COUNT);
+    PostgresConnection db(DB_CONNECTION_INFO);
+
+    PGresult* result = db.executeQuery("SELECT * FROM news ORDER BY created_at DESC LIMIT 1");
+
+    if (!result) {
+        fmt::print("[MAIN]: Ошибка при выполнении запроса к news.\n");
+        return 1;
+    }
+
+    int rows = PQntuples(result);
+    if (rows == 0) {
+        fmt::print("[MAIN]: В таблице news нет записей.\n");
+        db.clearResult(result);
+        return 0;
+    }
+
+    std::string id        = PQgetvalue(result, 0, PQfnumber(result, "id"));
+    std::string title     = PQgetvalue(result, 0, PQfnumber(result, "title"));
+
+
+    fmt::print("\n[ПОСЛЕДНЯЯ НОВОСТЬ]:\nID: {}\nЗаголовок: {}\n\n", id, title);
 
     bot.start();
 
